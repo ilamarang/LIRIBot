5ae07dc512477981c916535f70238fc8eda67a38
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-06-02 11:49:49 -0400

    Initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'JvIlM8R4Z0dJPCJCh5YNaxcvd',
+  consumer_secret: 'UHgdfyGUXBxONUEltl5z70fxNUaLLrPq8qfmWTH9Q0tDPpd2mL',
+  access_token_key: '870664426030018560-ql2xloBN7fMasufL3gNOWCv9ZRN1CRB',
+  access_token_secret: 'ScGDmQr6SnzbzkZZDtgnBoKKSMCxXO8IGGkYfPOAjXF4o',
+}
