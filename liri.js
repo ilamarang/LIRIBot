251043a6a3e4658d14f7886389cba1796e54e27d
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-06-03 04:40:16 -0400

    Added the framework and beautified code

--- liri.js
+++ liri.js
@@ -6,83 +6,131 @@ var request = require("request");
 //var fs = require("fs");
 var lineReader = require('line-reader');
 
-function spotifySong (trackName) {
+function spotifySong(trackName) {
+    spotify.search({
+        type: 'track',
+        query: trackName,
+        limit: 1
+    }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
-  spotify.search({ type: 'track', query: trackName,limit:1 }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
+        console.log("######## Hey - Here is your search results for Spotify ###########################\n");
 
-console.log("######## Hey - Here is your search results for Spotify ###########################\n");
+        var songReturned = data.tracks.items[0];
 
-var songReturned = data.tracks.items[0];
+        //console.log(JSON.stringify(data.tracks.items[0]));
 
-//console.log(JSON.stringify(data.tracks.items[0]));
+        songReturned.album.artists.forEach(function(artist, index) {
+            console.log("Artist(s): \n" + (index + 1) + ") " + artist.name)
 
-songReturned.album.artists.forEach(function(artist,index) {
-  console.log("Artist(s): \n" + (index+1) + ") " +  artist.name)
+        })
 
-})
+        console.log("Album Name: " + songReturned.album.name);
 
-console.log("Album Name: " + songReturned.album.name);
+        console.log("Song Name: " + songReturned.name);
 
-console.log("Song Name: " + songReturned.name);
+        console.log("Preview URL: " + songReturned.preview_url);
 
-console.log("Preview URL: " + songReturned.preview_url);
+        console.log("\n######## Done with Spotify #########################################################");
 
-console.log("\n######## Done with Spotify #########################################################");
-
-})
+    })
 };
 
+function fetchTweets() {
+    client.get('search/tweets', {
+        q: 'NodePianoMan',
+        count: 20
+    }, function(error, tweets, response) {
+        console.log("Here is my Tweets!")
+        tweets.statuses.forEach(function(tweet, index) {
+            console.log((index + 1) + ") " + tweet.text);
+        })
+
+        console.log("Closing Tweets!")
+    });
+
+}
+
+function convertRandomTextToArray() {
+  var dataArray = [];
+  lineReader.eachLine("./random.txt", function(line, last) {
+
+      dataArray.push(line);
+      if (last) {
+          getRandomSelection(dataArray);
+      }
+  });
+}
+
+function launchApp(instruction, data) {
+    switch (instruction) {
+        case "my-tweets":
+            fetchTweets();
+            break;
+        case "spotify-this-song":
+            spotifySong(data);
+            break;
+        case "do-what-it-says":
+            convertRandomTextToArray();
+            break;
+
+    }
+
+}
+
+
+
 function getRandomSelection(dataArray) {
-console.log(dataArray.length);
+    console.log(dataArray.length);
+
+    var randomNumber = Math.floor(Math.random() * dataArray.length)+1;
+    var randomInstruction = dataArray[randomNumber-1].split(",");
+
+    console.log(randomInstruction[0] + "  " + randomInstruction[1]);
+    launchApp(randomInstruction[0], randomInstruction[1]);
+
 }
 
 
 var client = new Twitter({
-  consumer_key: keys.twitterKeys.consumer_key,
-  consumer_secret: keys.twitterKeys.consumer_secret,
-  access_token_key: keys.twitterKeys.access_token_key,
-  access_token_secret: keys.twitterKeys.access_token_secret
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
 });
 
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify({
-  id: keys.spotifyKeys.client_id,
-  secret: keys.spotifyKeys.client_secret
+    id: keys.spotifyKeys.client_id,
+    secret: keys.spotifyKeys.client_secret
 });
 
-if(process.argv[2]=="my-tweets") {
-  client.get('search/tweets',{q: 'NodePianoMan', count:20},function(error, tweets, response) {
-    console.log("Here is my Tweets!")
-    tweets.statuses.forEach(function(tweet,index) {
-      console.log((index+1) + ") " + tweet.text);
-    })
-
-    console.log("Closing Tweets!")
-  });
-
-} else if(process.argv[2]=="spotify-this-song") {
-  var trackName = process.argv[3];
-  spotifySong(trackName);
-
-} else if(process.argv[2]=="movie-this") {
-console.log("Hello Movie");
-request('http://www.omdbapi.com/?i=tt3896198&apikey=87c66fec', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
-} else if(process.argv[2]=="do-what-it-says") {
-  var dataArray = [];
-  lineReader.eachLine("./random.txt", function(line, last) {
-
-  dataArray.push(line);
-  if(last){
-        getRandomSelection(dataArray);
-  }
-});
+launchApp(process.argv[2],process.argv[3]);
 
 
-}
+
+
+/*
+
+if (process.argv[2] == "my-tweets") {
+
+
+} else if (process.argv[2] == "movie-this") {
+    console.log("Hello Movie");
+    request('http://www.omdbapi.com/?i=tt3896198&apikey=87c66fec', function(error, response, body) {
+        console.log('error:', error); // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the HTML for the Google homepage.
+    });
+} else if (process.argv[2] == "do-what-it-says") {
+    var dataArray = [];
+    lineReader.eachLine("./random.txt", function(line, last) {
+
+        dataArray.push(line);
+        if (last) {
+            getRandomSelection(dataArray);
+        }
+    });
+*/
